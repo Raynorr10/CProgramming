13b0fbc35e4e556d0b2f13b49b56a9abcf9aca91
Bruno GONZALEZ <bruno.gonzalez@telecom-paristech.fr>
2018-01-18 20:09:46 +0100

    beginning TP7and8

---
+++ TP7and8/material/link-types.h
@@ -0,0 +1,10 @@
+typedef struct student {
+ char lastname[30];
+ char firstname[20];
+ int group;
+} Student_t;
+
+typedef struct link{
+     Student_t  student;
+     struct link * next;
+} Link_t;
